35f1af460ba609fc73ef815ede9bd32492a13ca6
Hervé Poussineau <hpoussin@reactos.org>
2022-07-10 15:46:22 +0200

    [KDROSDBG] Implement KdDebuggerInitialize0/KdDebuggerInitialize1
    We now support initialization of SERIAL, SCREEN and FILE devices.
    Code has been copied from ntoskrnl/kd/
    
    WARNING: FILE output has not been tested

--- drivers/base/kdrosdbg/kdrosdbg.h
+++ drivers/base/kdrosdbg/kdrosdbg.h
@@ -2,6 +2,10 @@
 #include <ntifs.h>
 #include <halfuncs.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <arc/arc.h>
 #include <windbgkd.h>
 #include <kddll.h>
+#include <cportlib/cportlib.h>
+#include <ndk/inbvfuncs.h>
+#include <drivers/bootvid/bootvid.h>
